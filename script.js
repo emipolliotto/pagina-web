8ca47d49a48fc992c6288efee836fc71ea6b9ad1
emipolliotto <epolliotto@itba.edu.ar>
2025-05-27 19:24:00 -0300

    actualizacion

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-document.querySelector('.btn-warning').addEventListener('click', function(event) {
-    event.preventDefault();  // Previene el comportamiento por defecto del enlace
-    const target = document.querySelector('#miPlanner');  // Selecciona la sección destino
-    target.scrollIntoView({
-        behavior: 'smooth',  // Desplazamiento suave
-        block: 'start'  // Asegura que el bloque esté al inicio
-    });
-});
+
+
