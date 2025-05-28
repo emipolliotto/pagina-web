f23ca3961ee3e6000c59e39f3cbc4aa1d3b6b7b9
emipolliotto <epolliotto@itba.edu.ar>
2025-05-27 21:56:25 -0300

    act2

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const links = document.querySelectorAll('[data-section]');
+  const secciones = document.querySelectorAll('.seccion');
 
+  links.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const target = link.getAttribute('data-section');
+
+      secciones.forEach(sec => sec.classList.add('d-none'));
+      document.getElementById(target).classList.remove('d-none');
+    });
+  });
 
